383c66e55fe76b61f83d5aed569b0e8f398bc456
cyril314 <svip.cyril@gmail.com>
2025-11-03 19:14:29 +0800

    update code

--- src/main/java/com/fit/AimApplication.java
+++ src/main/java/com/fit/AimApplication.java
@@ -2,12 +2,14 @@ package com.fit;
 
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -21,22 +23,19 @@ public class AimApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext run = SpringApplication.run(AimApplication.class, args);
         String ip = InetAddress.getLocalHost().getHostAddress();
-        String port = getParam("PORT", run.getEnvironment().getProperty("server.port"));
-
-        log.info("\n---------------------------------------------------------\n" +
-                "Application FreeAdmin is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:" + port + "/\n\t" +
-                "External:\thttp://" + ip + ":" + port + "/" +
-                "\n-----------------页面请部署 admin-web----------------------");
+        String port = System.getenv("POST");
+        ConfigurableEnvironment environment = run.getEnvironment();
+        if (Strings.isEmpty(port)) {
+            port = environment.getProperty("server.port");
+        }
+        String path = environment.getProperty("server.servlet.context-path", "").replace("/", "");
+        log.info("---------------------------------------------------------");
+        log.info("Access URLs:\n\tLocal: \t\thttp://localhost:{}/{}\n\tExternal:\t{}://{}:{}/{}", port, path, "http", ip, port, path);
+        log.info("---------------------- admin-web ------------------------");
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(AimApplication.class);
-    }
-
-    private static String getParam(String param, String defVal) {
-        Optional<String> envPort = Optional.ofNullable(System.getenv(param));
-        return envPort.map(String::valueOf).orElse(defVal);
     }
 }
